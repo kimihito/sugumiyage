820fabc6e9029c77eaf66e09e11e3789cf3494f6
kimihito <tatsurotamashiro@gmail.com>
2012-11-23 17:44:07 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sugumiyage::Application.config.secret_token = 'a26e04ff03b3e684ece95b29ffda721c3b4e0a8f1b29f83dfd4db470cefc771d3a69b28158a44f3de83885b54ed7bd36c655a72a2a5177873a856ed070be217b'
